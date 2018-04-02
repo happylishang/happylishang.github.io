b8d7ba5ba5b0ef0b6f852da9d6cffcb6ee93462f
happylishang <happylishang@163.com>
2018-04-02 15:23:57 +0800

    修改Android全下管理的内容

--- w3c/js/article.js
+++ w3c/js/article.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
       loadPosts(posts);
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
-      alert("Postfile的JSON格式化错误 错误文件 "+postfile + errorThrown);
+      alert("Postfile的JSON格式化错误 错误文件 "+ XMLHttpRequest + "  "+ postfile +"  "+ errorThrown);
     }
   });
 
